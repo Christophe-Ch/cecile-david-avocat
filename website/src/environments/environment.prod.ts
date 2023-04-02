1cc6f4f5b884a1dd8d55e52f66eb93a88cde6557
Christophe CHICHMANIAN <chris.chich@hotmail.fr>
2023-04-02 19:41:55 +0200

    :bug: Fixed wrong API url

--- website/src/environments/environment.prod.ts
+++ website/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  contactEndpoint: 'https://europe-west1-la-sophrologie-pas-a-pas.cloudfunctions.net/send-mail',
+  contactEndpoint: 'https://europe-west1-cecile-david.cloudfunctions.net/send-mail',
   recaptchaScriptUrl: 'https://www.google.com/recaptcha/enterprise.js?render=6LewGFElAAAAAHn0uMqVMoZLM_J94_Y8F0R8DWr9'
 };
