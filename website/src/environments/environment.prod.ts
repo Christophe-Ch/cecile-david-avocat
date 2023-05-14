856e2c50e28e934f4ca94e8a06b6d22576646e23
Christophe CHICHMANIAN <chris.chich@hotmail.fr>
2023-05-14 17:07:56 +0200

    :bug: Fixed prod contact endpoint

--- website/src/environments/environment.prod.ts
+++ website/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  contactEndpoint: 'https://europe-west1-cecile-david.cloudfunctions.net/send-mail',
+  contactEndpoint: 'https://europe-west1-cecile-david-site.cloudfunctions.net/send-mail',
   recaptchaScriptUrl: 'https://www.google.com/recaptcha/enterprise.js?render=6LcFYgomAAAAAMH7ZZMFkVdjYOgIHs9PnqA4Hz9S',
   recaptchaSiteKey: '6LcFYgomAAAAAMH7ZZMFkVdjYOgIHs9PnqA4Hz9S'
 };
